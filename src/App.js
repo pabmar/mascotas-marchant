bcbe45cb5d2096b7ead9a5df8cf91158720b4e21
Pablo Marchant <pabmar1290@gmail.com>
2021-06-13 12:46:41 -0400

    carpetas Tarea

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import NavBar from './components/NavBar/NavBar'
 import ItemCount from './components/ItemCount/ItemCount'
-
+import ItemListContainer from './components/ItemListContainer/ItemListContainer'
+import Item from './components/Item/Item'
 const App = props => {
   return <div className="appContainer">
     <div>
     <NavBar/>
     </div>
     <ItemCount/>
+    <ItemListContainer/>
+    <Item></Item>
   </div>
   
 }
@@ -17,7 +20,6 @@ export default App;
 
 
  
-
-
-
  
+
+
---
+++ src/components/Item/Item.js
@@ -0,0 +1,31 @@
+import React,{useEffect, useState} from 'react'
+import axios from 'axios'
+ 
+const Item = () =>{
+
+    const[listado, setListado] = useState([]);
+
+    const getListado = () =>{
+
+        axios.get('https://pokeapi.co/api/v2/pokemon?limit=10')
+         .then(response =>{
+             setListado(response.data.results)
+         })
+         .catch(error => console.log(error))
+    }
+    useEffect(() =>{
+        getListado()
+    },[])
+
+    
+    return (
+        <div>
+            <ul>
+                {listado.map((p,i)=> <li key={i}>{p.name}</li>)}
+            </ul>
+            
+        </div>
+    )
+}
+
+export default Item
