1faeca904d11112c53fa6bdb1a056380b313f079
Pablo Marchant <pabmar1290@gmail.com>
2021-06-20 13:11:41 -0400

    arreglos detail

--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -5,76 +5,102 @@ import ItemDetail from '../ItemDetail/ItemDetail';
 import Container from '@material-ui/core/Container';
 
 
+const dataProductos = () =>{
 
-const data = [
-  {
-    img: 'https://image.freepik.com/foto-gratis/comida-perro_74190-4075.jpg',
-    titulo:'Alimento 1',
-    alt:'alimento 1',
-    resumen:'este es el primer alimento',
-    precio:'1990',
-    categoria:'Alimento',
-    infoAdicional:'aca esta la informacion adicional'
+  return new Promise ((resolve,reject) =>{
 
-  },
-  {
-    img: 'https://image.freepik.com/foto-gratis/comida-perro_74190-4075.jpg',
-    titulo:'Alimento 2',
-    alt:'alimento 2',
-    resumen:'este es el 2do alimento',
-    precio:'2100',
-    categoria:'Alimento',
-    infoAdicional:'aca esta la informacion adicional'
-    
+    setTimeout(() => {
+      resolve(
+        [
+          {
+            id:1,
+            img: 'https://image.freepik.com/foto-gratis/comida-perro_74190-4075.jpg',
+            titulo:'Alimento 1',
+            alt:'alimento 1',
+            resumen:'este es el primer alimento',
+            precio:'1990',
+            categoria:'Alimento',
+            infoAdicional:'aca esta la informacion adicional'
+        
+          },
+          {
+            id:2,
+            img: 'https://image.freepik.com/foto-gratis/comida-perro_74190-4075.jpg',
+            titulo:'Alimento 2',
+            alt:'alimento 2',
+            resumen:'este es el 2do alimento',
+            precio:'2100',
+            categoria:'Alimento',
+            infoAdicional:'aca esta la informacion adicional'
+            
+         
+          },
+          {
+              id:3,
+              img: 'https://image.freepik.com/foto-gratis/comida-perro_74190-4075.jpg',
+              titulo:'Alimento 3',
+              alt:'alimento 3',
+              resumen:'este es el 3er alimento',
+              precio:'2300'
+              ,categoria:'Alimento',
+              infoAdicional:'aca esta la informacion adicional'
+          },
+          {
+              id:4,
+              img: 'https://image.freepik.com/foto-gratis/comida-perro_74190-4075.jpg',
+              titulo:'Alimento 4',
+              alt:'alimento 4',
+              resumen:'este es el primer alimento',
+              precio:'1990'
+              ,categoria:'Alimento',
+              infoAdicional:'aca esta la informacion adicional'
+         
+            },
+            {
+              id:5,
+              img: 'https://cdn.pixabay.com/photo/2018/03/20/13/25/dog-bowl-3243272_960_720.jpg',
+              titulo:'juguete 1',
+              alt:'juguete 1',
+              resumen:'este es el primer juguete',
+              precio:'2100',
+              categoria:'Juguete',
+              infoAdicional:'aca esta la informacion adicional'
+           
+            },
+            {
+                id:6,
+                img: 'https://cdn.pixabay.com/photo/2018/03/20/13/25/dog-bowl-3243272_960_720.jpg',
+                titulo:'juguete 2',
+                alt:'juguete 2',
+                resumen:'este es el 2do juguete',
+                precio:'2300',
+                categoria:'Juguete',
+                infoAdicional:'aca esta la informacion adicional'
+            }
+        ])
+    },3000);
  
-  },
-  {
-      img: 'https://image.freepik.com/foto-gratis/comida-perro_74190-4075.jpg',
-      titulo:'Alimento 3',
-      alt:'alimento 3',
-      resumen:'este es el 3er alimento',
-      precio:'2300'
-      ,categoria:'Alimento',
-      infoAdicional:'aca esta la informacion adicional'
-  },
-  {
-      img: 'https://image.freepik.com/foto-gratis/comida-perro_74190-4075.jpg',
-      titulo:'Alimento 4',
-      alt:'alimento 4',
-      resumen:'este es el primer alimento',
-      precio:'1990'
-      ,categoria:'Alimento',
-      infoAdicional:'aca esta la informacion adicional'
- 
-    },
-    {
-      img: 'https://cdn.pixabay.com/photo/2018/03/20/13/25/dog-bowl-3243272_960_720.jpg',
-      titulo:'juguete 1',
-      alt:'juguete 1',
-      resumen:'este es el primer juguete',
-      precio:'2100',
-      categoria:'Juguete',
-      infoAdicional:'aca esta la informacion adicional'
-   
-    },
-    {
-        img: 'https://cdn.pixabay.com/photo/2018/03/20/13/25/dog-bowl-3243272_960_720.jpg',
-        titulo:'juguete 2',
-        alt:'juguete 2',
-        resumen:'este es el 2do juguete',
-        precio:'2300',
-        categoria:'Juguete',
-        infoAdicional:'aca esta la informacion adicional'
-    }
-]
+  })
 
+} 
  
 
 const ItemDetailContainer = props => {
  
+  const [productos, setProductos] = useState([])
+
+  const llamarProductos  = () =>{
+    dataProductos().then(data =>{const dataMostrar = data.filter(data => (data.id == 1)) 
+      console.log(dataMostrar)
+    setProductos(dataMostrar)})
     
-    const producto = data.[0]
-    console.log(producto)
+  }
+  useEffect(() =>{
+    llamarProductos();
+     
+  },[])
+    const producto = productos[0]
+ 
     return (
             
           <Container>
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -17,66 +17,72 @@ const dataProductos = () =>{
     setTimeout(() => {
       resolve(
         [
-        {
-          img: 'https://image.freepik.com/foto-gratis/comida-perro_74190-4075.jpg',
-          titulo:'Alimento 1',
-          alt:'alimento 1',
-          resumen:'este es el primer alimento',
-          precio:'1990',
-          categoria:'Alimento',
-          infoAdicional:'aca esta la informacion adicional'
-      
-        },
-        {
-          img: 'https://image.freepik.com/foto-gratis/comida-perro_74190-4075.jpg',
-          titulo:'Alimento 2',
-          alt:'alimento 2',
-          resumen:'este es el 2do alimento',
-          precio:'2100',
-          categoria:'Alimento',
-          infoAdicional:'aca esta la informacion adicional'
-          
-       
-        },
-        {
+          {
+            id:1,
             img: 'https://image.freepik.com/foto-gratis/comida-perro_74190-4075.jpg',
-            titulo:'Alimento 3',
-            alt:'alimento 3',
-            resumen:'este es el 3er alimento',
-            precio:'2300'
-            ,categoria:'Alimento',
-            infoAdicional:'aca esta la informacion adicional'
-        },
-        {
-            img: 'https://image.freepik.com/foto-gratis/comida-perro_74190-4075.jpg',
-            titulo:'Alimento 4',
-            alt:'alimento 4',
+            titulo:'Alimento 1',
+            alt:'alimento 1',
             resumen:'este es el primer alimento',
-            precio:'1990'
-            ,categoria:'Alimento',
+            precio:'1990',
+            categoria:'Alimento',
             infoAdicional:'aca esta la informacion adicional'
-       
+        
           },
           {
-            img: 'https://cdn.pixabay.com/photo/2018/03/20/13/25/dog-bowl-3243272_960_720.jpg',
-            titulo:'juguete 1',
-            alt:'juguete 1',
-            resumen:'este es el primer juguete',
+            id:2,
+            img: 'https://image.freepik.com/foto-gratis/comida-perro_74190-4075.jpg',
+            titulo:'Alimento 2',
+            alt:'alimento 2',
+            resumen:'este es el 2do alimento',
             precio:'2100',
-            categoria:'Juguete',
+            categoria:'Alimento',
             infoAdicional:'aca esta la informacion adicional'
+            
          
           },
           {
+              id:3,
+              img: 'https://image.freepik.com/foto-gratis/comida-perro_74190-4075.jpg',
+              titulo:'Alimento 3',
+              alt:'alimento 3',
+              resumen:'este es el 3er alimento',
+              precio:'2300'
+              ,categoria:'Alimento',
+              infoAdicional:'aca esta la informacion adicional'
+          },
+          {
+              id:4,
+              img: 'https://image.freepik.com/foto-gratis/comida-perro_74190-4075.jpg',
+              titulo:'Alimento 4',
+              alt:'alimento 4',
+              resumen:'este es el primer alimento',
+              precio:'1990'
+              ,categoria:'Alimento',
+              infoAdicional:'aca esta la informacion adicional'
+         
+            },
+            {
+              id:5,
               img: 'https://cdn.pixabay.com/photo/2018/03/20/13/25/dog-bowl-3243272_960_720.jpg',
-              titulo:'juguete 2',
-              alt:'juguete 2',
-              resumen:'este es el 2do juguete',
-              precio:'2300',
+              titulo:'juguete 1',
+              alt:'juguete 1',
+              resumen:'este es el primer juguete',
+              precio:'2100',
               categoria:'Juguete',
               infoAdicional:'aca esta la informacion adicional'
-          }
-      ])
+           
+            },
+            {
+                id:6,
+                img: 'https://cdn.pixabay.com/photo/2018/03/20/13/25/dog-bowl-3243272_960_720.jpg',
+                titulo:'juguete 2',
+                alt:'juguete 2',
+                resumen:'este es el 2do juguete',
+                precio:'2300',
+                categoria:'Juguete',
+                infoAdicional:'aca esta la informacion adicional'
+            }
+        ])
     },3000);
  
   })
@@ -101,7 +107,7 @@ const ItemListContainer = props => {
  
     return <>
           {productos.length == 0?(
-            <Box display="flex" justifyContent="center">
+            <Box display="flex" justifyContent="center  ">
               <CircularProgress />
            </Box>
           ):(
